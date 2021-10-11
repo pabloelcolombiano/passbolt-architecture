d5e9ceecadd8feabc6cf5ca73cb1489b59462880
Juan Pablo Ramirez <pabloelcolombiano@gmail.com>
2021-10-11 23:16:52 +0200

    Composer works

--- bootstrap.php
+++ bootstrap.php
@@ -4,5 +4,5 @@ use App\Application;
 
 require_once dirname(__DIR__) . '/passbolt-architecture/vendor/autoload.php';
 
-$client = is_string($argv[1]) ? $argv[1] : 'ce';
+$client = isset($argv[1]) ? $argv[1] : 'ce';
 $app = new Application($client);
