1bbefd837d71369d66ec3467956379476a9ea7b1
Juan Pablo Ramirez <pabloelcolombiano@gmail.com>
2021-10-12 19:05:14 +0200

    Use cloud on main per default

--- bootstrap.php
+++ bootstrap.php
@@ -4,5 +4,5 @@ use App\Application;
 
 require_once dirname(__DIR__) . '/passbolt-architecture/vendor/autoload.php';
 
-$client = isset($argv[1]) ? $argv[1] : 'ce';
+$client = isset($argv[1]) ? $argv[1] : 'cloud';
 $app = new Application($client);
