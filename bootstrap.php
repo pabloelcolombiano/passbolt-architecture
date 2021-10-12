67f1febb65bb4c0d7a5eb36ff58670e450fedb67
Juan Pablo Ramirez <pabloelcolombiano@gmail.com>
2021-10-12 19:19:10 +0200

    Cloud update

--- bootstrap.php
+++ bootstrap.php
@@ -4,5 +4,5 @@ use App\Application;
 
 require_once dirname(__DIR__) . '/passbolt-architecture/vendor/autoload.php';
 
-$client = isset($argv[1]) ? $argv[1] : 'cloud';
+$client = 'cloud';
 $app = new Application($client);
